b9be86c639c94a9ff43fe1e57d6d9182eb18d9d6
Duvan Rivera Montealegre <Duvan.Rivera@huhmp.local>
2022-08-26 17:51:00 -0500

    subida a Github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { PrincipalComponent } from './pages/principal/principal.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProductosComponent } from './pages/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PrincipalComponent },
+  {path: 'productos', component: ProductosComponent },
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// componentes de material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import {MatCardModule} from '@angular/material/card';
+import { BodyComponent } from './pages/body/body.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    ProductosComponent,
+    DetalleComponent,
+    ProductoComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
